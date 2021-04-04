897e3ba486f07de27c6608c42e3d4e911b251faa
arturo-caceres <arturo.caceres02383@gmail.com>
2021-04-04 12:32:04 +1000

    Adding main_view.rb

---
+++ main_view.rb
@@ -0,0 +1,27 @@
+require 'rainbow'
+
+
+puts " "
+puts " "
+puts " "
+puts " "
+puts " "
+puts Rainbow("                     Are you smarter than a 5th grader?").color(17, 168, 205).bright
+puts " "
+puts " "
+puts " "
+puts " "
+puts " "
+puts "Welcome to game that gives grown ups to chance to prove that you are smarter than a 5th grader. "
+puts " "
+puts ""
+puts " Please select an option:"
+puts ""
+puts ""
+puts "Start Game"
+puts ""
+puts "About"
+puts ""
+puts "Exit Game"
+puts ""
+
