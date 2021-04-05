ab1211c4192ac7465a82fb5b5b58170a0d35f816
arturo-caceres <arturo.caceres02383@gmail.com>
2021-04-05 21:39:41 +1000

    Adding changes to files

--- about.rb
+++ about.rb
@@ -1,20 +1,13 @@
 require 'rainbow'
 
 
-puts " "
-puts " "
-puts " "
-puts " "
-puts Rainbow("                     Are you smarter than a 5th grader?").color(17, 168, 205).bright
-puts " "
-puts " "
-puts ""
-puts ""
-puts Rainbow("About").underline.bright
-puts ""
-puts "Are you smarter than a 5th grader is a quiz game. The Player will be asked a series of 10 questions, which are various subjects(such as Science, Math, World Geography, ect.) taken from elementary school textbooks. Each correct answer increases the amount of points you can accumulate, reaching 100 points and winning the game."
-puts ""
-puts ""
-puts "Go back to menu ->"
-puts ""
-puts ""
+def about_game
+    puts ""
+    puts Rainbow("About").underline.bright
+    puts ""
+    puts "Are you smarter than a 5th grader is a quiz game. The Player will be asked a series of 10 questions, which are various subjects(such as Science, Math, World Geography, ect.) taken from elementary school textbooks. Each correct answer increases the amount of points you can accumulate, reaching 100 points and winning the game."
+    puts ""
+    puts ""
+
+end
+
--- start.rb
+++ start.rb
@@ -1,7 +1,9 @@
 require 'rainbow'
 
+require_relative "./about.rb"
+require_relative "./game.rb"
 
-puts " "
+
 puts " "
 puts " "
 puts " "
@@ -9,71 +11,31 @@ puts " "
 puts Rainbow("                     Are you smarter than a 5th grader?").color(17, 168, 205).bright
 puts " "
 puts " "
-puts " "
-puts " "
-puts " "
-puts " Lets begin!"
-puts ""
-print " Please enter your name: "
-user_name = gets.chomp.capitalize
-puts ""
-print " Please enter your age too: "
-age = gets.chomp
-puts ""
-puts " Thank you #{user_name}! "
-puts ""
-puts ""
-puts Rainbow("Instructions").green.underline.bright
-puts ""
-puts " Every question is worth 10 points. Your goal is to reach 100 points without failing a question. "
-puts " If you fail, you will start from the beginning."
-puts " "
-puts " Are you ready to show everyone how easy these questions are?"
-puts " "
-puts ""
-game_points = 0
-puts "Current points = " + game_points.to_s
-puts " "
-puts ""
-game_questions = [{
-    "question" => "how old are you?",
-    "options" => ["op1","op2","op3","op5"],
-    "answer" => "op1"
-}, 
-{"question" => "age?",
-"options" => ["op1","op2","op3","op5"],
-"answer" => "op1"},
-{"question" => "Color of the sky?",
-"options" => ["op1","op2","op3","op5"],
-"answer" => "op3"}]
 
-game_questions.each { |question_hash| 
-
-puts question_hash["question"] 
+puts " "
+puts " "
+puts "Welcome to the game that gives grown ups to chance to prove that you are smarter than a 5th grader. "
+puts " "
 puts ""
-puts question_hash["options"]
+puts " Please select an option:"
+puts ""
+puts ""
+puts "Start Game (Press 1 for this option)"
+puts ""
+puts "About (Press 2 for this option)"
+puts ""
+puts "Exit Game (Press 3 for this option)"
 puts ""
 
-user_input = Integer(gets.chomp)
-user_answer = question_hash["options"][user_input-1]
-expected_answer = question_hash["answer"]
-puts ""
-if user_answer == expected_answer 
-    puts "correct" 
-    game_points += 10
-    if game_points == 30
-        puts "Con-drag-tulation! You are #1"
-        break
-    end
-else 
-    puts "incorrect, start again."
-    game_points = 0
-    puts "Game over"
-    break
+option = Integer(gets.chomp)
+
+if option == 1
+    start_game
+elsif option == 2
+    about_game 
+else option == 3
+    puts "Bye"
 end
-puts "Current points " + game_points.to_s 
-puts ""
-}
 
 
 
