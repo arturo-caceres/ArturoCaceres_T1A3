f3625398142fa6f1936cc13c7bf9f9870d750e70
arturo-caceres <arturo.caceres02383@gmail.com>
2021-04-08 20:29:40 +1000

    Adding player.rb

---
+++ player.rb
@@ -0,0 +1,15 @@
+
+
+
+class Player
+    def initialize(name, age) 
+        @name = name
+        @age = age
+    end
+
+    def thank_player
+        return "Thank you #{@age}year old #{@name}"
+    end
+
+end
+
