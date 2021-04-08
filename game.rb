aff486340faeb682038e00a1fc389295053921a7
arturo-caceres <arturo.caceres02383@gmail.com>
2021-04-08 20:30:27 +1000

    Updating game.rb file

--- game.rb
+++ game.rb
@@ -1,5 +1,16 @@
-require './questions.rb'
+# require './questions.rb'
 
+class Player
+    def initialize(name, age) 
+        @name = name
+        @age = age
+    end
+
+    def thank_player
+        return "Thank you #{@age}year old #{@name}"
+    end
+
+end
 
 def start_game
 
@@ -19,21 +30,23 @@ def start_game
 
     puts "
  
- 
- 
- 
     "
+
     puts " Lets begin!
     "
+
+
+
     print " Please enter your name: "
     user_name = gets.chomp.capitalize
     puts ""
     print " Please enter your age too: "
     age = gets.chomp
-    puts ""
-    puts " Thank you #{user_name}! 
+   
+    p1 = Player.new(user_name,age)
 
-    "
+    puts p1.thank_player
+    
     puts Rainbow("Instructions").green.underline.bright
     puts ""
     puts " Every question is worth 10 points. Your goal is to reach 100 points without failing a question. "
@@ -53,36 +66,37 @@ def start_game
 
     Questions::GAME_QUESTIONS.each { |question_hash| 
 
-    puts question_hash["question"] 
-    puts ""
-    puts question_hash["options"]
-    puts "
+        puts question_hash["question"] 
+        puts ""
+        puts question_hash["options"]
+        puts "
 
-    "
-    user_input = Integer(gets.chomp)
-    user_answer = question_hash["options"][user_input-1]
-    expected_answer = question_hash["answer"]
-    
-    if user_answer == expected_answer 
-        puts "correct" 
-        puts "" 
-        game_points += 10
-        if game_points == 30
-            puts game_points.to_s
-            puts "Congratulations! You are #1"
+        "
+        user_input = Integer(gets.chomp)
+        user_answer = question_hash["options"][user_input-1]
+        expected_answer = question_hash["answer"]
+        
+        if user_answer == expected_answer 
+            puts "correct" 
+            puts "" 
+            game_points += 10
+            if game_points == 30
+                puts game_points.to_s
+                puts "Congratulations! You are #1"
+                break
+            end
+        else 
+            puts "Incorrect. "
+            puts "Wow, at your age you should know this. It's time to grab a book my friend. "
+            puts "" 
+            game_points = 0 
+            puts "Game over. Start again."
+            puts ""
             break
         end
-    else 
-        puts "Incorrect. "
-        puts "Wow, at your age you should know this. It's time to grab a book my friend. "
-        puts "" 
-        game_points = 0 
-        puts "Game over. Start again."
+        puts "Current points " + game_points.to_s 
         puts ""
-        break
-    end
-    puts "Current points " + game_points.to_s 
-    puts ""
+
     }
 
     # while true do
