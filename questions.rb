f3a2814e7dfcf4f436d4d21f1b6f93eb7dcea77b
arturo-caceres <arturo.caceres02383@gmail.com>
2021-04-08 10:47:56 +1000

    adding questions.rb

---
+++ questions.rb
@@ -0,0 +1,13 @@
+module Questions
+    GAME_QUESTIONS = [{
+        "question" => "In the sentence 'Questlove is a very good drummer'. What part of speech is the word 'very'?",
+        "options" => ["Adverb","Noun","Verb","Adjective"],
+        "answer" => "Adverb"
+    }, 
+    {"question" => "In our solar system, which planet is farthest from the sun?",
+    "options" => ["Neptune","Pluto","Saturn","Uranus"],
+    "answer" => "Neptune"},
+    {"question" => "In miles, approximately how far is the earth from the sun?",
+    "options" => [86_000,90_000,94_000,98_000],
+    "answer" => 90_000}]
+end
