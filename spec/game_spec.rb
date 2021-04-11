bd76057d9ada1eb417015a34277c2c59f4c6c976
arturo-caceres <arturo.caceres02383@gmail.com>
2021-04-11 18:50:32 +1000

    Updating all files

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,18 +1,16 @@
-require './player.rb'
-
+require "../src/player.rb"
 
 # RSpec.describe "Player" do
 #     subject { Player.new }
 
 #     it "should respond to name" do
-#         expect(subject).to respond_to :name 
+#         expect(subject).to respond_to :name
 #     end
 
 #     describe "name" do
 #         it "should not be nil" do
 #             expect(subject.name).not_to be_nil
 #         end
-   
 
 #         it "should be assign on initilisation if provided" do
 #             player = Player.new "Arturo"
@@ -21,16 +19,14 @@ require './player.rb'
 #     end
 # end
 
-
 Player = Struct.new(:name, :age)
 
 RSpec.describe Player.new("Arturo", 31) do
-   
-    it { is_expected.to have_attributes(:name => "Arturo", :age => 31) }
-    it { is_expected.to have_attributes(:name => "Arturo") }
-    it { is_expected.to have_attributes(:age => 31) }
-    it { is_expected.to have_attributes(:age => (a_value >= 7) ) }
-    it { is_expected.to have_attributes(:age => (a_value <= 99) ) }
-    it { is_expected.not_to have_attributes(:name => "Bob") }
-    it { is_expected.not_to have_attributes(:age => 5) }
+  it { is_expected.to have_attributes(:name => "Arturo", :age => 31) }
+  it { is_expected.to have_attributes(:name => "Arturo") }
+  it { is_expected.to have_attributes(:age => 31) }
+  it { is_expected.to have_attributes(:age => (a_value >= 7)) }
+  it { is_expected.to have_attributes(:age => (a_value <= 99)) }
+  it { is_expected.not_to have_attributes(:name => "Bob") }
+  it { is_expected.not_to have_attributes(:age => 5) }
 end
